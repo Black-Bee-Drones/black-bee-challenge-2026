b1a43a78118ccffe6d21feedb37f0c6c86bedfec
Pedro Bevi <pedrobevilaqua04@gmail.com>
2026-07-10 14:32:33 -0300

    Added task package

---
+++ precision_landing/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'precision_landing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pedro-bevilaqua',
+    maintainer_email='pedrobevilaqua04@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
