a96a412e8ed80d1fa60668a2f6ba55d1dc0712d7
artxavier <arthuraxribeiro@gmail.com>
2026-07-22 14:25:36 -0300

    Created basic sub state machine for hanging the hook

---
+++ hang_the_hook/hang_the_hook/hookSM/hookSM.py
@@ -0,0 +1,45 @@
+import time
+import yasmin
+from yasmin import State, StateMachine, Blackboard
+from yasmin_ros.basic_outcomes import SUCCEED, ABORT
+
+from states import Align, Descend, Hook, EndHook
+
+class hookSM(StateMachine):
+    def __init__(self):
+        super().__init__(outcomes=[SUCCEED, ABORT])
+
+        self.add_state(
+            "ALIGN",
+            Align(),
+            transitions={
+                SUCCEED: "DESCEND",
+                ABORT: ABORT          
+            }
+        )
+        self.add_state(
+            "DESCEND",
+            Descend(),
+            transitions={
+                SUCCEED: "DROP_HOOK",
+                ABORT: "ALIGN"        
+            }
+        )
+        self.add_state(
+            "DROP_HOOK",
+            Hook(),
+            transitions={
+                SUCCEED: "END_HOOK",
+                ABORT: "ALIGN"        
+            }
+        )
+        self.add_state(
+            "END_HOOK",
+            EndHook(),
+            transitions={
+                SUCCEED: SUCCEED,     
+                ABORT: ABORT          
+            }
+        )
+
+        self.set_start_state("ALIGN")
