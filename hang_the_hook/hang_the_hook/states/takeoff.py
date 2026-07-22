450e42eb82355a9040e129e66788597707a6ab4d
artxavier <arthuraxribeiro@gmail.com>
2026-07-22 19:48:34 -0300

    Deleted states folder for modularization in two different sub state machines and created core folder for implementing the basic states

--- hang_the_hook/hang_the_hook/states/takeoff.py
+++
@@ -1,7 +0,0 @@
-import yasmin
-from yasmin import State, Blackboard
-from yasmin_ros.basic_outcomes import SUCCEED, ABORT
-from yasmin_ros.yasmin_node import YasminNode
-
-class Takeoff(State):
-    pass
