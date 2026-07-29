44b8b4a19b01f72d154363f2d60cd7227202131a
Pedro Bevi <pedrobevilaqua04@gmail.com>
2026-07-29 10:36:30 -0300

    feat: Initialize State and some constants

--- precision_landing/precision_landing/constants.py
+++ precision_landing/precision_landing/constants.py
@@ -1,1 +1,10 @@
 # File that will have all the constants we will use in the task
+SIM_MODE = True
+
+CAMERA_SOURCE = '/down_camera'
+PHOTOS_FOLDER = '' #Folder where we will save the frames for later processing
+
+IMAGE_WIDTH = 640 #NOTE: Need to be certain about this ones
+IMAGE_HEIGHT = 640
+
+TAKEOFF_HEIGHT = 5 #Meters, can change this later
