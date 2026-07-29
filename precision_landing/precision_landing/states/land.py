92d4f9e552d106b99ae3ec739b27d73f680446a1
Pedro Bevi <pedrobevilaqua04@gmail.com>
2026-07-29 13:25:13 -0300

    feat: Takeoff and Land states

--- precision_landing/precision_landing/states/land.py
+++ precision_landing/precision_landing/states/land.py
@@ -1,7 +1,30 @@
 import yasmin
 from yasmin import State, Blackboard
 from yasmin_ros.basic_outcomes import SUCCEED, ABORT
-from yasmin_ros.yasmin_node import YasminNode
+
+from nectar.control import MavrosDrone
 
 class Land(State):
-    pass
+    def __init__(self):
+        super().__init__(outcomes=[SUCCEED, ABORT])
+
+    def execute(self, blackboard: Blackboard):
+        if "drone" not in blackboard:
+            yasmin.YASMIN_LOG_ERROR("Drone not available...")
+            return ABORT
+
+        drone: MavrosDrone = blackboard["drone"]
+
+        try:
+            yasmin.YASMIN_LOG_INFO("LANDING...")
+
+            drone.land()
+            drone.delay(2)
+            
+            yasmin.YASMIN_LOG_INFO("LANDING completed.")
+
+            return SUCCEED
+
+        except Exception as e:
+            yasmin.YASMIN_LOG_ERROR(f"LANDING Failed: {e}")
+            return ABORT
--- precision_landing/precision_landing/states/takeoff.py
+++ precision_landing/precision_landing/states/takeoff.py
@@ -1,7 +1,36 @@
 import yasmin
 from yasmin import State, Blackboard
 from yasmin_ros.basic_outcomes import SUCCEED, ABORT
-from yasmin_ros.yasmin_node import YasminNode
+
+from nectar.control import MavrosDrone
+
+from constants import (
+    TAKEOFF_HEIGHT,
+)
 
 class Takeoff(State):
-    pass
+    def __init__(self):
+        super().__init__(outcomes=[SUCCEED, ABORT])
+
+    def execute(self, blackboard: Blackboard):
+        if "drone" not in blackboard:
+            yasmin.YASMIN_LOG_ERROR("Drone not available...")
+            return ABORT
+
+        drone: MavrosDrone = blackboard["drone"]
+
+        try:
+            yasmin.YASMIN_LOG_INFO("Starting TAKEOFF...")
+
+            drone.set_home() #Sets the current gps position as 'home'
+            drone.arm()
+            drone.takeoff(TAKEOFF_HEIGHT, max_retries=3, timeout=30.0, precision=0.2)
+            drone.delay(1)
+            
+            yasmin.YASMIN_LOG_INFO("TAKEOFF completed.")
+
+            return SUCCEED
+        
+        except Exception as e:
+            yasmin.YASMIN_LOG_ERROR(f"TAKEOFF Failed: {e}")
+            return ABORT
