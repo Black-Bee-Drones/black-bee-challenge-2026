26da86ef6be817ed75bdee7cff8f257b5cc30c10
jotavio <jozf0506@outlook.com>
2026-08-04 13:47:04 -0300

    feat: add takeoff state and Config class in constants

--- package_delivery/package_delivery/constants.py
+++ package_delivery/package_delivery/constants.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 # Simulation
 SIM_MODE = 1
 SIM_IMAGE_SOURCE = "/down_camera"
@@ -6,3 +7,57 @@ SIM_IMAGE_COMPRESSED = False
 # Camera
 IMAGE_WIDTH = 0     # Change later
 IMAGE_HEIGHT = 0    # Change later
+
+
+@dataclass
+class Config:
+    
+    safe_altitude: float = 2.0 # meters
+    max_altitude: float = 6.0  # meters
+    
+    drone_type: str = 'mavlink'
+    connection_string: str = 'udp:127.0.0.1:14551'
+    
+    # Takeoff
+    takeoff_altitude: float = safe_altitude
+    
+    # Center
+    center_threshold_xy: float = 0.2  # meters
+    center_threshold_z: float = 0.2  # meters
+    center_threshold_yaw: float = 5.0  # degrees
+    lost_tolerance: int = 5
+    land_altitude: float = 1.0  # meters
+    
+    # waypoints: launch and package bases; boxes
+    launch_bases: list[dict] = []
+    pkgs_bases: list[dict] = []
+    delivery_boxes: list[dict] = []
+    
+
+    ### PIDController ###
+    # PID xy
+    controller_xy_kp: float = 1.0
+    controller_xy_kd: float = 1.0
+    controller_xy_ki: float = 1.0
+    controller_xy_output_min: float = -1.0
+    controller_xy_output_max: float = 1.0
+    controller_xy_integral_min: float = -1.0
+    controller_xy_integral_max: float = 1.0
+
+    # PID z
+    controller_z_kp: float = 1.0
+    controller_z_kd: float = 1.0
+    controller_z_ki: float = 1.0
+    controller_z_output_min: float = -1.0
+    controller_z_output_max: float = 1.0
+    controller_z_integral_min: float = -1.0
+    controller_z_integral_max: float = 1.0
+
+    # PID yaw
+    controller_yaw_kp: float = 1.0
+    controller_yaw_kd: float = 1.0
+    controller_yaw_ki: float = 0
+    controller_yaw_output_min: float = -1.0
+    controller_yaw_output_max: float = 1.0
+    controller_yaw_integral_min: float = -1.0
+    controller_yaw_integral_max: float = 1.0
--- package_delivery/package_delivery/states/takeoff.py
+++ package_delivery/package_delivery/states/takeoff.py
@@ -4,6 +4,33 @@ from yasmin import State, Blackboard
 from yasmin_ros.basic_outcomes import SUCCEED, ABORT
 from yasmin_ros.yasmin_node import YasminNode
 
+from nectar.control import MavrosDrone
+
+from package_delivery.constants import Config
+
 
 class Takeoff(State):
-    pass
+    def __init__(self, config: Config):
+        super().__init__(outcomes=[SUCCEED, ABORT])
+
+        self.config = config
+
+    def execute(self, blackboard: Blackboard):
+        drone: MavrosDrone = blackboard.get('drone')
+
+        yasmin.YASMIN_LOG_INFO(
+            f'Taking off to altitude: {self.config.takeoff_altitude} m...')
+
+        try:
+            drone.takeoff(self.config.takeoff_altitude)
+
+        except KeyboardInterrupt:
+            yasmin.YASMIN_LOG_WARN('Execution interrupted by user.')
+            return ABORT
+
+        except Exception as e:
+            yasmin.YASMIN_LOG_ERROR(f'Taking off failed: {e}')
+            return ABORT
+
+        yasmin.YASMIN_LOG_INFO('Completed successfully.')
+        return SUCCEED
