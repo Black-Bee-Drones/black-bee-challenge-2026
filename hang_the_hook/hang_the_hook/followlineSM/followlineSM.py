5a37b2ca306c1fb1db63343a9d5204d35b7c6348
artxavier <arthuraxribeiro@gmail.com>
2026-08-08 14:59:01 -0300

    feat(followlineSM): add new Yasmin return outcomes for state transitions

--- hang_the_hook/hang_the_hook/followlineSM/followlineSM.py
+++ hang_the_hook/hang_the_hook/followlineSM/followlineSM.py
@@ -1,3 +1,4 @@
+from constants import FOUND_RED, FOUND_BLUE
 from yasmin import StateMachine
 from yasmin_ros.basic_outcomes import SUCCEED, ABORT
 
@@ -8,20 +9,12 @@ class FollowLineSM(StateMachine):
         super().__init__(outcomes=[SUCCEED, ABORT])
 
         self.add_state(
-            "SETUP_LINE_DETECTION",
-            SetupLineDetection(),
-            transitions={
-                SUCCEED: "SEARCH_BLUE_LINE",
-                ABORT: ABORT
-            }
-        )
-
-        self.add_state(
             "SEARCH_BLUE_LINE",
             SearchBlueLine(),
             transitions={
-                SUCCEED: "FOLLOW_BLUE_LINE",
-                ABORT: "CHECK_END_FOLLOW_LINE"
+                FOUND_BLUE: "FOLLOW_BLUE_LINE",
+                FOUND_RED: SUCCEED,  # Transition to SUCCEED if red line is found
+                ABORT: ABORT,
             }
         )
 
@@ -29,9 +22,9 @@ class FollowLineSM(StateMachine):
             "FOLLOW_BLUE_LINE",
             FollowBlueLine(),
             transitions={
-                SUCCEED: "ALIGN",      ##ver com o marco isso aq dps    
-                ABORT: "SEARCH_BLUE_LINE"
+                SUCCEED: "SEARCH_BLUE_LINE", 
+                ABORT: ABORT,
             }
         )
 
-        self.set_start_state("SETUP_LINE_DETECTION")
+        self.set_start_state("SEARCH_BLUE_LINE")
