7c83b022a6f3ca60d6baf0581cf68787e74cc933
artxavier <arthuraxribeiro@gmail.com>
2026-08-24 17:58:44 -0300

    test(colorspace): changed color space again for solving code running issue

--- hang_the_hook/hang_the_hook/core/states.py
+++ hang_the_hook/hang_the_hook/core/states.py
@@ -73,7 +73,7 @@ class Initialize(State):
             )
 
             hosedetector = LineDetector(
-                color="blue",
+                color="yellow",
                 estimation_method=RotatedRect(),
                 color_space=ColorSpace.HSV
             )
