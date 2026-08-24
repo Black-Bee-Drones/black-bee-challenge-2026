938e1c7a19557df737f452cafb52f7288802935b
artxavier <arthuraxribeiro@gmail.com>
2026-08-24 17:41:32 -0300

    test(colorspace): changed colorspace to try to find issue for line detection

--- hang_the_hook/hang_the_hook/core/states.py
+++ hang_the_hook/hang_the_hook/core/states.py
@@ -73,7 +73,7 @@ class Initialize(State):
             )
 
             hosedetector = LineDetector(
-                color="red_hose",
+                color="green",
                 estimation_method=RotatedRect(),
                 color_space=ColorSpace.HSV
             )
