32556ea2e2fa0d2117637157aa72b367802e656c
artxavier <arthuraxribeiro@gmail.com>
2026-08-24 18:02:24 -0300

    fix: solved camera opening issue

--- hang_the_hook/hang_the_hook/followlineSM/states.py
+++ hang_the_hook/hang_the_hook/followlineSM/states.py
@@ -85,6 +85,7 @@ class SearchBlueLine(State):
                 print("One or more detectors or image handler not initialized.")
                 return ABORT
 
+            camera.open()
             last_detection_time = time()
 
             while True:
@@ -145,6 +146,9 @@ class SearchBlueLine(State):
         except Exception as e:
             print(f"Blue line searching failed: {e}")
             return ABORT
+        finally:
+            if camera:
+                camera.close()
 
 class SeekLine(State):
     '''
