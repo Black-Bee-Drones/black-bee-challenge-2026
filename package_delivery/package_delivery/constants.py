99dd1eedaf065d9670d65eb3a8a11ce8275a8313
Ana Machado <ana.machado8002@gmail.com>
2026-08-26 14:03:02 -0300

    fix: update box lat and long

--- package_delivery/package_delivery/constants.py
+++ package_delivery/package_delivery/constants.py
@@ -11,8 +11,8 @@ class Config:
     # )
     
     target_box: tuple = (
-        (-22.415338, -45.447946),     # box 1: lat, long
-        (-22.415377, -45.447988),     # box 2: lat, long
+        (-22.4153391, -45.4479390),     # box 1: lat, long
+        (-22.4153820, -45.4479658),     # box 2: lat, long
         (2, 2),     # box 3: lat, long
     )
 
