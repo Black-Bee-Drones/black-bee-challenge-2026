684dd3bb39253c6da529d52fddfcc00ae077d162
jotavio <jozf0506@outlook.com>
2026-08-26 12:08:53 -0300

    test without servo - states commented

--- package_delivery/package_delivery/mangalarga.py
+++ package_delivery/package_delivery/mangalarga.py
@@ -28,14 +28,14 @@ class PackageDelivery(StateMachine):
         self.add_state(
             "INITIALIZE",
             Initialize(),
-            transitions={SUCCEED: "GRIP_PKG", ABORT: ABORT}
-        )
-        
-        self.add_state(
-            "GRIP_PKG",
-            Gripper(target_has_pkg=True),
             transitions={SUCCEED: "TAKEOFF", ABORT: ABORT}
         )
+        
+        # self.add_state(
+        #     "GRIP_PKG",
+        #     Gripper(target_has_pkg=True),
+        #     transitions={SUCCEED: "TAKEOFF", ABORT: ABORT}
+        # )
 
         self.add_state(
             "TAKEOFF",
@@ -53,14 +53,14 @@ class PackageDelivery(StateMachine):
         self.add_state(
             "APPROACH",
             Approach(),
-            transitions={SUCCEED: "DROP_PKG", ABORT: "RTL"}
+            transitions={SUCCEED: "RTL", ABORT: "LAND"}
         )
 
-        self.add_state(
-            "DROP_PKG",
-            Gripper(target_has_pkg=False),
-            transitions={SUCCEED: "RTL", ABORT: ABORT}
-        )
+        # self.add_state(
+        #     "DROP_PKG",
+        #     Gripper(target_has_pkg=False),
+        #     transitions={SUCCEED: "RTL", ABORT: ABORT}
+        # )
         
         self.add_state(
             "LAND",
