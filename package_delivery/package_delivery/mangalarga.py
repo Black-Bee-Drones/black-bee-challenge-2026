e5237d895982bec8b0a997b83b4c7bf808f35050
jotavio <jozf0506@outlook.com>
2026-08-27 17:50:58 -0300

    fix: final state machine

--- package_delivery/package_delivery/mangalarga.py
+++ package_delivery/package_delivery/mangalarga.py
@@ -28,14 +28,14 @@ class PackageDelivery(StateMachine):
         self.add_state(
             "INITIALIZE",
             Initialize(),
-            transitions={SUCCEED: "TAKEOFF", ABORT: ABORT}
+            transitions={SUCCEED: "GRIP_PKG", ABORT: ABORT}
         )
         
-        # self.add_state(
-        #     "GRIP_PKG",
-        #     Gripper(target_has_pkg=True),
-        #     transitions={SUCCEED: "TAKEOFF", ABORT: ABORT}
-        # )
+        self.add_state(
+            "GRIP_PKG",
+            Gripper(target_has_pkg=True),
+            transitions={SUCCEED: "TAKEOFF", ABORT: ABORT}
+        )
 
         self.add_state(
             "TAKEOFF",
@@ -53,14 +53,14 @@ class PackageDelivery(StateMachine):
         self.add_state(
             "APPROACH",
             Approach(),
-            transitions={SUCCEED: "RTL", ABORT: "LAND"}
+            transitions={SUCCEED: "DROP_PKG", ABORT: "LAND"}
         )
 
-        # self.add_state(
-        #     "DROP_PKG",
-        #     Gripper(target_has_pkg=False),
-        #     transitions={SUCCEED: "RTL", ABORT: ABORT}
-        # )
+        self.add_state(
+            "DROP_PKG",
+            Gripper(target_has_pkg=False),
+            transitions={SUCCEED: "RTL", ABORT: ABORT}
+        )
         
         self.add_state(
             "LAND",
