53c9f014e7e13a0bb800ec08cdd518c51605315d
Pedro Bevi <pedrobevilaqua04@gmail.com>
2026-08-27 17:06:06 -0300

    Esqueci de voltar o yaw

--- precision_landing/precision_landing/findSM/states.py
+++ precision_landing/precision_landing/findSM/states.py
@@ -86,11 +86,13 @@ class Search(State): #Sub-state that will only move around the arena until it de
                             yasmin.YASMIN_LOG_INFO(f"{i}")
                             drone.move_to(yaw=yaw_angle)
                             drone.move_to(x=1, reference=MoveReference.BODY)
+                            
                             frame = camera.take_photo()
                             bbox2, id = aruco.detect(frame.image)
 
                             aruco_shape = self.get_aruco_shape(frame, bbox2)
-
+                        
+                        drone.move_to(yaw=-yaw_angle)
                         blackboard["aruco_shape"] = aruco_shape
                         yasmin.YASMIN_LOG_INFO(f"Aruco shape detected: {aruco_shape}")
                     
