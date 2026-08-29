9e54b83b4462bd6ce2de633073c79a806e8b781a
artxavier <arthuraxribeiro@gmail.com>
2026-08-29 13:05:23 -0300

    fix: takeoff altitude adjust set to false

--- hang_the_hook/hang_the_hook/core/states.py
+++ hang_the_hook/hang_the_hook/core/states.py
@@ -164,7 +164,7 @@ class Takeoff(State):
 
         try:
             YASMIN_LOG_INFO(f"Taking off to {TAKEOFF_HEIGHT}m...")
-            ok = self.drone.takeoff(altitude=TAKEOFF_HEIGHT, max_retries=5)
+            ok = self.drone.takeoff(altitude=TAKEOFF_HEIGHT, max_retries=5, adjust_altitude=False)
             ok = self.drone.move_to(x=0.0,y=0.0,z=(2.0 - self.drone.get_altitude()))
 
             YASMIN_LOG_INFO("Takeoff complete.")
